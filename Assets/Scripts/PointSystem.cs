ba43e4d0b76b1148048590ebe2badc3fe86aa48c
King Zhou <zhouyimingking@outlook.com>
2022-01-07 16:21:51 -0800

    Main scene UI layout and point system functionality

--- Assets/Scripts/PointSystem.cs
+++ Assets/Scripts/PointSystem.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using TMPro;
@@ -8,15 +6,20 @@ using UnityEngine.UI;
 
 public class PointSystem : MonoBehaviour
 {
+    [SerializeField] private const int MaxPointsAvailable = 10;
     private int Happiness { get; set; }
     private int Sadness { get; set; }
     private int Fear { get; set; }
     private int Anger { get; set; }
+    private int PointsAvailable { get; set; }
 
     private List<TextMeshProUGUI> _valueList;
+    private TextMeshProUGUI _pointsAvailabelText;
 
     private void Start()
     {
+        PointsAvailable = MaxPointsAvailable;
+        _pointsAvailabelText = GameObject.Find("PointsAvailable").GetComponent<TextMeshProUGUI>();
         _valueList = GetComponentsInChildren<TextMeshProUGUI>().ToList()
             .FindAll(ugui => ugui.CompareTag("PointSystem.Value"));
 
@@ -30,6 +33,8 @@ public class PointSystem : MonoBehaviour
 
     private void UpdateValue(string valueName, bool plus)
     {
+        if ((PointsAvailable == MaxPointsAvailable && !plus) || (PointsAvailable == 0 && plus)) return;
+        
         var text = _valueList.Find(ugui => ugui.name == valueName);
         var increment = plus ? 1 : -1;
 
@@ -52,5 +57,8 @@ public class PointSystem : MonoBehaviour
                 text.text = Anger.ToString();
                 break;
         }
+
+        PointsAvailable -= increment;
+        _pointsAvailabelText.text = PointsAvailable.ToString();
     }
 }
